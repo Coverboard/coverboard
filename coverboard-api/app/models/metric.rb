ce1cf22562829b28d4a3d4bf896b685c0cb80895
mfranczakhelpling <michal.franczak@helpling.com>
2015-12-11 14:00:25 +0100

    Metric name validation

--- coverboard-api/app/models/metric.rb
+++ coverboard-api/app/models/metric.rb
@@ -1,5 +1,9 @@
 class Metric < ActiveRecord::Base
+  NAMES = %w{coverage coverage-models coverage-controllers}
+
   belongs_to :project
 
   validates :project, presence: true
+
+  validates :name, inclusion: Metric::NAMES
 end
