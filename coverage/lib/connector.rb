cc259a3fb6e25bce68b53d6b4265f0b5482c72c3
Lautaro Mazzitelli <lautaro.mazzitelli@helpling.com>
2015-12-11 15:49:47 +0100

    Tidyness after a day of writing shit.

--- coverage/lib/connector.rb
+++ coverage/lib/connector.rb
@@ -2,44 +2,27 @@ require 'net/http'
 
 class Connector
 
+  attr_accessor :payload
+  attr_accessor :endpoint
+  attr_accessor :response
+  attr_accessor :user
+  attr_accessor :pass
+  attr_accessor :host
+  attr_accessor :port
+  attr_accessor :auth_enabled
+
   def initialize
-    @post_ws = '/api/metrics'
+    @endpoint = '/api/metrics'
 
-    @user = 'user@socialtext.com'
-    @pass = 'password'
+    @user = 'admin@helpling.com'
+    @pass = 'cleanmeup'
     @host = 'localhost'
-    @port = '80'
-  end
-
-
-  def set_auth credentials
-    @user = credentials['user']
-    @pass = credentials['password']
-  end
-
-  def set_auth_enabled enabled=false
-    @auth_enabled = enabled
-  end
-
-  def set_connection conn
-    @host = conn[:host]
-    @port = conn[:port]
-  end
-
-  def set_payload payload
-    @payload = payload
-  end
-
-  def set_endpoint endpoint
-    @post_ws = endpoint
-  end
-
-  def response
-    @response
+    @port = '3000'
+    @auth_enabled = false
   end
 
   def post
-    req = Net::HTTP::Post.new(@post_ws, initheader = {'Content-Type' =>'application/json'})
+    req = Net::HTTP::Post.new(@endpoint, initheader = {'Content-Type' =>'application/json'})
     req.basic_auth @user, @pass if @auth_enabled
     req.body = @payload
     response = Net::HTTP.new(@host, @port).start {|http| http.request(req) }
