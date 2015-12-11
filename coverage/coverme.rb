f15569d043113dfee22fa74de3b84ed5497a5e6b
Lautaro Mazzitelli <lautaro.mazzitelli@helpling.com>
2015-12-11 14:52:29 +0100

    First working draft

--- coverage/coverme.rb
+++ coverage/coverme.rb
@@ -3,19 +3,43 @@
 require 'rubygems'
 require 'nokogiri'
 
+require './lib/application_message'
+require './lib/connector'
+
 # TODO change for a command line parameter
 INDEX_HTML_LOC = '/Users/lautaromazzitelli/Projects/hassle/coverage/index.html'
+UID = 'myCoveredApplication'
+TIMESTAMP = nil
 
 def parse_index_file
 
-  doc = File.open(INDEX_HTML_LOC ) { |f| Nokogiri::HTML(f) }
-  
-  nodes = doc.xpath('//*[@id="content"]/div/h2')
+  prefix = 'coverage-'
 
+  metrics = Hash.new
+  doc = File.open(INDEX_HTML_LOC ) { |f| Nokogiri::HTML(f) }
+  nodes = doc.xpath('//*[@id="content"]/div/h2')
   nodes.each do |node|
-    puts node.xpath('span[@class="group_name"]').inner_html
-    puts node.xpath('span[@class="covered_percent"]/span').inner_html
+    key =  (prefix + node.xpath('span[@class="group_name"]').inner_html).gsub(' ','-').downcase
+    value = node.xpath('span[@class="covered_percent"]/span').inner_html.chomp('%').to_f
+    metrics[key] = value
   end
+
+  metrics
+end
+
+def create_message metrics
+    app_message = ApplicationMessage.new
+    app_message.set_uid UID
+    app_message.set_timestamp TIMESTAMP
+    app_message.set_metrics metrics
+    return app_message.to_json
+end
+
+def send_to_api msg
+  connector = Connector.new
+  connector.set_payload msg
+  connector.post
+  puts connector.response
 end
 
 ## Parse command line parameters
@@ -27,7 +51,8 @@ end
 ## Main entry point
 def main
   parse_command_line
-  parse_index_file
+  msg = create_message parse_index_file
+  send_to_api msg
 end
 
 main
