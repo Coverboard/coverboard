458b635d74884ea0d34c1668d9a50888f959ae63
mfranczakhelpling <michal.franczak@helpling.com>
2015-12-11 11:23:04 +0100

    Application and Metric data models

--- coverboard-api/db/seeds.rb
+++ coverboard-api/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Application.create name: 'sample', uid: 'qwerty123456'
