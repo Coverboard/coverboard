e56915a1d7c445cf786887f45f208dd2b6e66c84
mfranczakhelpling <michal.franczak@helpling.com>
2015-12-16 22:13:12 +0100

    Project title added to the dashboard, fixes in the layout

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,13 @@
 class DashboardController < ApplicationController
   def index
+    @project_name = get_project_name
   end
+
+  private
+    def get_project_name
+      project = Project.find_by_uid(params[:project])
+      return '' if project.nil?
+
+      project.name
+    end
 end
